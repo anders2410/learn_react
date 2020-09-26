c26ffbbe53e228c5f86d744e5f66a10fa8c683d9
Anders Holt Knudsen <andhk11@gmail.com>
2020-09-26 11:50:41 +0200

    Beginning the movie app!

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
-  state: { count: number } = {
-    count: 0
+  state: { count: number; tags: string[] } = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
+  }
+
+  getBadgeClass(): string {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    return count === 0 ? "Zero" : count;
   }
 }
