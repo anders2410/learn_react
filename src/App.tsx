013d3e2c0ff3a34a99b9adbe739eec5780808e51
Anders Holt Knudsen <andhk11@gmail.com>
2020-10-12 23:08:16 +0200

    Finally solved the navbar problem..

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/common/not-found";
+import NavBar from "./components/common/nav-bar";
 
+/* The routing works as follows. The Switch component makes sure only one
+ *  URL is matched. Then the Route renders the components associated with the
+ *  URL-path. And then Redirect makes sure only legal URL's are used, and otherwise
+ *  it send the user to a NotFound page.
+ * */
 export default function App() {
   return (
-    <main className="container">
-      <Route path="/movies" component={Movies} />
-      <Route path="/customers" component={Customers} />
-      <Route path="/rentals" component={Rentals} />
-      <Route path="/not-found" component={NotFound} />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
